035a36f18616d8c3fcfccab4630e794151227023
katsikas <katsikas@csd.uoc.gr>
2012-05-04 16:51:29 +0300

    all

---
+++ Gnuradio/QAM64.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+##################################################
+# Gnuradio Python Flow Graph
+# Title: QAM64
+# Author: katsikas
+# Generated: Thu May  3 17:17:05 2012
+##################################################
+
+from gnuradio import digital
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio.eng_option import eng_option
+from gnuradio.gr import firdes
+from grc_gnuradio import blks2 as grc_blks2
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import wx
+
+class QAM64(grc_wxgui.top_block_gui):
+
+	def __init__(self):
+		grc_wxgui.top_block_gui.__init__(self, title="QAM64")
+
+		##################################################
+		# Variables
+		##################################################
+		self.samp_rate = samp_rate = 44100
+
+		##################################################
+		# Blocks
+		##################################################
+		self.gr_wavfile_source_0 = gr.wavfile_source("/home/katsikas/guitarup_full.wav", True)
+		self.gr_wavfile_sink_0 = gr.wavfile_sink("/home/katsikas/katsikas-repo/Gnuradio/Sent.wav", 1, samp_rate, 8)
+		self.gr_throttle_0_0 = gr.throttle(gr.sizeof_float*1, samp_rate)
+		self.digital_ofdm_mod_0 = grc_blks2.packet_mod_f(digital.ofdm_mod(
+				options=grc_blks2.options(
+					modulation="qam256",
+					fft_length=2048,
+					occupied_tones=200,
+					cp_length=128,
+					pad_for_usrp=True,
+					log=None,
+					verbose=None,
+				),
+			),
+			payload_length=0,
+		)
+		self.digital_ofdm_demod_0 = grc_blks2.packet_demod_f(digital.ofdm_demod(
+				options=grc_blks2.options(
+					modulation="qam256",
+					fft_length=2048,
+					occupied_tones=200,
+					cp_length=128,
+					snr=10,
+					log=None,
+					verbose=None,
+				),
+				callback=lambda ok, payload: self.digital_ofdm_demod_0.recv_pkt(ok, payload),
+			),
+		)
+
+		##################################################
+		# Connections
+		##################################################
+		self.connect((self.gr_wavfile_source_0, 0), (self.gr_throttle_0_0, 0))
+		self.connect((self.digital_ofdm_mod_0, 0), (self.digital_ofdm_demod_0, 0))
+		self.connect((self.gr_throttle_0_0, 0), (self.digital_ofdm_mod_0, 0))
+		self.connect((self.digital_ofdm_demod_0, 0), (self.gr_wavfile_sink_0, 0))
+
+	def get_samp_rate(self):
+		return self.samp_rate
+
+	def set_samp_rate(self, samp_rate):
+		self.samp_rate = samp_rate
+
+if __name__ == '__main__':
+	parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
+	(options, args) = parser.parse_args()
+	tb = QAM64()
+	tb.Run(True)
+
