b77a7d811e55185d55ee08bc27d057472e8cc51f
katsikas <katsikas@csd.uoc.gr>
2012-06-08 11:36:15 +0300

    all

--- Gnuradio/DVB-T/QAM16-Extended.py
+++ Gnuradio/DVB-T/QAM16-Extended.py
@@ -10,12 +10,22 @@ from PyQt4 import Qt
 from gnuradio import digital
 from gnuradio import eng_notation
 from gnuradio import gr
+
+#########################
+from gnuradio import atsc
+#########################
+
 from gnuradio.eng_option import eng_option
 from gnuradio.gr import firdes
 from gnuradio.qtgui import qtgui
 from grc_gnuradio import blks2 as grc_blks2
 from optparse import OptionParser
 import PyQt4.Qwt5 as Qwt
+
+#############
+import random
+#############
+
 import sip
 import sys
 
@@ -49,6 +59,22 @@ class QAM16(gr.top_block, Qt.QWidget):
 		self.OFDM_Symbols = OFDM_Symbols = 2048
 		self.Guard_Interval = Guard_Interval = 4
 		self.Gain = Gain = 20
+		
+		##################################################
+		# My Variables
+		self.Packets_Number = 2048
+		##################################################
+
+
+		##################################################
+		# My code...
+		print(atsc.ATSC_MPEG_PKT_LENGTH);
+		print(atsc.MPEG_SYNC_BYTE);
+		self.ts = self.make_fake_transport_stream_packet(self.Packets_Number) # make 2048 packets of 188 each one.
+		print(len(self.ts))
+		##################################################
+
+
 
 		##################################################
 		# Blocks
@@ -84,7 +110,13 @@ class QAM16(gr.top_block, Qt.QWidget):
 		self._qtgui_sink_x_0_win = sip.wrapinstance(self.qtgui_sink_x_0.pyqwidget(), Qt.QWidget)
 		self.top_layout.addWidget(self._qtgui_sink_x_0_win)
 		self.gr_throttle_0 = gr.throttle(gr.sizeof_gr_complex*1, samp_rate)
-		self.gr_file_source_0 = gr.file_source(gr.sizeof_char*1, "/home/katsikas/katsikas-repo/Gnuradio/Common/Text.txt", True)
+
+		#########################################################################################
+		# My Blocks
+		#self.gr_file_source_0 = gr.file_source(gr.sizeof_char*1, "/home/katsikas/katsikas-repo/Gnuradio/Common/Text.txt", True)
+		self.gr_vector_source_0 = gr.vector_source_b(self.ts, True, 1)
+		#########################################################################################
+
 		self.gr_file_sink_0 = gr.file_sink(gr.sizeof_char*1, "/home/katsikas/katsikas-repo/Gnuradio/Common/Results.txt")
 		self.gr_file_sink_0.set_unbuffered(False)
 		self.digital_ofdm_mod_0 = grc_blks2.packet_mod_b(digital.ofdm_mod(
@@ -120,8 +152,12 @@ class QAM16(gr.top_block, Qt.QWidget):
 		self.connect((self.gr_throttle_0, 0), (self.qtgui_sink_x_0, 0))
 		self.connect((self.digital_ofdm_mod_0, 0), (self.gr_throttle_0, 0))
 		self.connect((self.digital_ofdm_mod_0, 0), (self.digital_ofdm_demod_0, 0))
-		self.connect((self.gr_file_source_0, 0), (self.digital_ofdm_mod_0, 0))
 		self.connect((self.digital_ofdm_demod_0, 0), (self.gr_file_sink_0, 0))
+		
+		###########################################################################
+		# My Connections
+		self.connect((self.gr_vector_source_0, 0), (self.digital_ofdm_mod_0, 0))
+		###########################################################################
 
 	def get_variable_qtgui_range_0(self):
 		return self.variable_qtgui_range_0
@@ -168,6 +204,32 @@ class QAM16(gr.top_block, Qt.QWidget):
 	def set_Gain(self, Gain):
 		self.Gain = Gain
 
+
+	########################################################################################
+	# My Functions
+	def make_fake_transport_stream_packet(self,npkts):
+    		"""
+    		Return a sequence of 8-bit ints that represents an MPEG Transport Stream packet.
+
+    		@param npkts: how many 188-byte packets to return
+
+    		FYI, each ATSC Data Frame contains two Data Fields, each of which contains
+    		312 data segments.  Each transport stream packet maps to a data segment.
+    		"""
+    		r = [0] * (npkts * 188)
+    		i = 0
+    		for j in range(npkts):
+        		r[i+0] = atsc.MPEG_SYNC_BYTE
+        		r[i+1] = random.randint(0, 127) # top bit (transport error bit) clear
+        		i = i + 2
+        		for n in range(186):
+            			r[i + n] = random.randint(0, 255)
+        		i = i + 186
+
+    		return r
+	########################################################################################
+
+
 if __name__ == '__main__':
 	parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
 	(options, args) = parser.parse_args()
@@ -177,4 +239,5 @@ if __name__ == '__main__':
 	tb.show()
 	qapp.exec_()
 	tb.stop()
+
 
--- Gnuradio/DVB-T/lampros.py
+++ Gnuradio/DVB-T/lampros.py
@@ -3,11 +3,15 @@
 # Gnuradio Python Flow Graph
 # Title: QAM16
 # Author: katsikas
-# Generated: Thu Jun  7 16:38:15 2012
+# Generated: Wed Jun  6 18:13:24 2012
 ##################################################
 
 from PyQt4 import Qt
 from gnuradio import digital
+#########################
+from gnuradio import atsc
+from gnuradio.atsc import *
+#########################
 from gnuradio import eng_notation
 from gnuradio import gr
 from gnuradio.eng_option import eng_option
@@ -16,8 +20,10 @@ from gnuradio.qtgui import qtgui
 from grc_gnuradio import blks2 as grc_blks2
 from optparse import OptionParser
 import PyQt4.Qwt5 as Qwt
+import random
 import sip
 import sys
+
 
 class QAM16(gr.top_block, Qt.QWidget):
 
@@ -49,6 +55,20 @@ class QAM16(gr.top_block, Qt.QWidget):
 		self.OFDM_Symbols = OFDM_Symbols = 2048
 		self.Guard_Interval = Guard_Interval = 4
 		self.Gain = Gain = 20
+		self.Packets_Number = 2048
+
+		
+
+		
+		##################################################
+		# My code...
+		print(atsc.ATSC_MPEG_PKT_LENGTH);
+		print(atsc.MPEG_SYNC_BYTE);
+		self.ts = self.make_fake_transport_stream_packet(self.Packets_Number) # make 2048 packets of 188 each one.
+		print(len(self.ts))
+		##################################################		
+	
+	
 
 		##################################################
 		# Blocks
@@ -84,10 +104,11 @@ class QAM16(gr.top_block, Qt.QWidget):
 		self._qtgui_sink_x_0_win = sip.wrapinstance(self.qtgui_sink_x_0.pyqwidget(), Qt.QWidget)
 		self.top_layout.addWidget(self._qtgui_sink_x_0_win)
 		self.gr_throttle_0 = gr.throttle(gr.sizeof_gr_complex*1, samp_rate)
-		self.gr_file_source_0 = gr.file_source(gr.sizeof_char*1, "/home/katsikas/katsikas-repo/Gnuradio/Common/Text.txt", True)
-		self.gr_file_sink_0 = gr.file_sink(gr.sizeof_char*1, "/home/katsikas/katsikas-repo/Gnuradio/Common/Results.txt")
+		#self.gr_file_source_0 = gr.file_source(gr.sizeof_float*1, "/home/katsikas/katsikas-repo/Gnuradio/Common/Text.txt", True)
+		self.gr_vector_source_0 = gr.vector_source_f(self.ts, True, 1)
+		self.gr_file_sink_0 = gr.file_sink(gr.sizeof_float*1, "/home/katsikas/katsikas-repo/Gnuradio/Common/Results.txt")
 		self.gr_file_sink_0.set_unbuffered(False)
-		self.digital_ofdm_mod_0 = grc_blks2.packet_mod_b(digital.ofdm_mod(
+		self.digital_ofdm_mod_0 = grc_blks2.packet_mod_f(digital.ofdm_mod(
 				options=grc_blks2.options(
 					modulation="qam64",
 					fft_length=OFDM_Symbols,
@@ -100,7 +121,7 @@ class QAM16(gr.top_block, Qt.QWidget):
 			),
 			payload_length=0,
 		)
-		self.digital_ofdm_demod_0 = grc_blks2.packet_demod_b(digital.ofdm_demod(
+		self.digital_ofdm_demod_0 = grc_blks2.packet_demod_f(digital.ofdm_demod(
 				options=grc_blks2.options(
 					modulation="qam64",
 					fft_length=OFDM_Symbols,
@@ -120,7 +141,8 @@ class QAM16(gr.top_block, Qt.QWidget):
 		self.connect((self.gr_throttle_0, 0), (self.qtgui_sink_x_0, 0))
 		self.connect((self.digital_ofdm_mod_0, 0), (self.gr_throttle_0, 0))
 		self.connect((self.digital_ofdm_mod_0, 0), (self.digital_ofdm_demod_0, 0))
-		self.connect((self.gr_file_source_0, 0), (self.digital_ofdm_mod_0, 0))
+		#self.connect((self.gr_file_source_0, 0), (self.digital_ofdm_mod_0, 0))
+		self.connect((self.gr_vector_source_0, 0), (self.digital_ofdm_mod_0, 0))
 		self.connect((self.digital_ofdm_demod_0, 0), (self.gr_file_sink_0, 0))
 
 	def get_variable_qtgui_range_0(self):
@@ -167,6 +189,28 @@ class QAM16(gr.top_block, Qt.QWidget):
 
 	def set_Gain(self, Gain):
 		self.Gain = Gain
+
+	def make_fake_transport_stream_packet(self,npkts):
+    		"""
+    		Return a sequence of 8-bit ints that represents an MPEG Transport Stream packet.
+
+    		@param npkts: how many 188-byte packets to return
+
+    		FYI, each ATSC Data Frame contains two Data Fields, each of which contains
+    		312 data segments.  Each transport stream packet maps to a data segment.
+    		"""
+    		r = [0] * (npkts * 188)
+    		i = 0
+    		for j in range(npkts):
+        		r[i+0] = atsc.MPEG_SYNC_BYTE
+        		r[i+1] = random.randint(0, 127) # top bit (transport error bit) clear
+        		i = i + 2
+        		for n in range(186):
+            			r[i + n] = random.randint(0, 255)
+        		i = i + 186
+
+    		return r
+
 
 if __name__ == '__main__':
 	parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
