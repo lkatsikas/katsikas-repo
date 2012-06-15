e0db76fc2826c185071bfca83e025606f99e65d7
katsikas <katsikas@csd.uoc.gr>
2012-06-15 20:35:55 +0300

    all

--- My-Gnuradio/DVB-T/Random_Ts.py
+++ My-Gnuradio/DVB-T/Random_Ts.py
@@ -63,17 +63,8 @@ class QAM16(gr.top_block, Qt.QWidget):
 		##################################################
 
 		
-		#--------------------------------------------------------------------------------------------------------------#
-		#with open('/home/katsikas/katsikas-repo/Gnuradio/Common/Text.txt', 'rb') as f:
-		with open('/home/katsikas/katsikas-repo/Simulink/Korgialas_GeiaSou.ts', 'rb') as f:
-			print("dvbt_source()")
-			self.ts = dvbt.dvbt_source()
-		#	print(self.dvb)
-		#	self.ts = make_fake_transport_stream_packet(Packets_Number,f) 
-		#	self.ts = pad_stream(self.ts, 256, 68)
-		#--------------------------------------------------------------------------------------------------------------#
-	
-
+		a = dvbt.pad()
+		print(a)
 		##################################################
 		# Blocks
 		##################################################
@@ -108,7 +99,12 @@ class QAM16(gr.top_block, Qt.QWidget):
 		self._qtgui_sink_x_0_win = sip.wrapinstance(self.qtgui_sink_x_0.pyqwidget(), Qt.QWidget)
 		self.top_layout.addWidget(self._qtgui_sink_x_0_win)
 		self.gr_throttle_0 = gr.throttle(gr.sizeof_gr_complex*1, samp_rate)
-		self.gr_vector_source_0 = gr.vector_source_b(self.ts,False,1)
+		#--------------------------------------------------------------------------------------------------------------#
+                #with open('/home/katsikas/katsikas-repo/Gnuradio/Common/Text.txt', 'rb') as file:
+			#self.gr_vector_source_0 = gr.vector_source_b(self.ts,False,1)
+                with open('/home/katsikas/katsikas-repo/Simulink/Korgialas_GeiaSou.ts', 'rb') as file:
+                        self.gr_vector_source_0 = dvbt.dvbt_source(file,self.Packets_Number)
+                #--------------------------------------------------------------------------------------------------------------#
 		self.gr_file_sink_0 = gr.file_sink(gr.sizeof_char*1, "/home/katsikas/katsikas-repo/My-Gnuradio/Common/Results.ts")
 		self.gr_file_sink_0.set_unbuffered(False)
 		self.digital_ofdm_mod_0 = grc_blks2.packet_mod_b(digital.ofdm_mod(
